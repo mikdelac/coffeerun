5713441cbbaefe4c0b111f7b5ab1615c8e9b2768
MikDelac <mikdelac@hotmail.com>
2020-11-15 18:24:45 -0500

    CH10 completed

---
+++ extract.js
@@ -0,0 +1,16 @@
+var path = require('path');
+
+var extractFilePath = function (url) {
+  var filePath;
+  var fileName = 'index.html';
+
+  if (url.length > 1) {
+    fileName = url.substring(1);
+  }
+  console.log('The filename is: ' + fileName);
+
+  filePath = path.resolve(__dirname, fileName);
+  return filePath;
+};
+
+module.exports = extractFilePath;
